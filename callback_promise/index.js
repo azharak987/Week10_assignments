53db61d2d2e7b705ee1f7bf8cef6642b295788c5
Azhar Ali Khan <azhar987k@gmail.com>
2023-09-23 14:54:07 +0500

    Updated Example

--- callback_promise/index.js
+++ callback_promise/index.js
@@ -1,35 +1,82 @@
-// Function to add two numbers and call a callback
-function addAsyncCallback(a, b, callback) {
-    function afterAddition(sum) {
-        callback(sum);
-    }
+function carMaintenance1(cb) {
+    console.log("I am leaving a car to you");
     setTimeout(() => {
-        const sum = a + b;
-        afterAddition(sum);
-    }, 1000);
+        console.log("car is ready");
+        cb("Your car is ready");
+    }, 2000);
 }
-function callbackHandler(result) {
-    console.log("Callback Example: The sum is:", result);
+function carMaintCallBack(text) {
+    console.log("car maintenance callback: ", text);
+    pickDress1(dressCallBack);
 }
-addAsyncCallback(2, 3, callbackHandler);
-// Function to add two numbers and return a Promise
-function addAsyncPromise(a, b) {
-    return new Promise((resolve) => {
-        function afterAddition(sum) {
-            resolve(sum);
-        }
-        setTimeout(() => {
-            const sum = a + b;
-            afterAddition(sum);
-        }, 1000);
-    });
+function pickDress1(callback) {
+    setTimeout(() => {
+        console.log("your dress is ready");
+        callback("pick your dress");
+    }, 100);
 }
-function promiseHandler(result) {
-    console.log("Promise Example: The sum is:", result);
+function dressCallBack(text) {
+    console.log("This is Dress Callback   ", text);
+    attendEvent1();
 }
-addAsyncPromise(2, 3)
-    .then(promiseHandler)
-    .catch((error) => {
-    console.error("Promise Example: Error:", error);
-});
+function attendEvent1() {
+    console.log("Now you can attend the event");
+}
+carMaintenance1(carMaintCallBack);
 export {};
+// console.log("\n\n\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n")
+//=======================================================================================
+// console.log("Promise Example")
+// //Promises
+// //Inversion of Control
+// function carMaintenance(){
+//     console.log("I am leaving a car to you")
+//     let result: Promise<string> = new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             // console.log( "car is ready")
+//             resolve("your car is ready")
+//             //reject("Your car is not ready")
+//         }, 2000);
+//     })
+//     return result;
+// }
+// function pickDress(){
+//     let promise: Promise<string> = new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             console.log("your dress is ready")
+//             resolve("pick your dress")
+//         }, 100);
+//     })
+//     return promise;
+// }
+// function attendEvent(){
+//     let promise: Promise<string> = new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             console.log("Attending Event");
+//             resolve("Attending Event")
+//         },2000);
+//     })
+//     return promise;
+// }
+// function goHome(){
+//     setTimeout(()=>{
+//         console.log("Going Home");
+//     },2000);
+// }
+// let mechanicResponse = carMaintenance();
+// mechanicResponse.then((text: string)=>{  //Then for Resolve value
+//     console.log("Mechanics Response: ", text)
+//     return pickDress();
+// })
+// .then((text: string)=>{
+//     console.log("Laundary Response: ",text);
+//     return attendEvent()
+// })
+// .then((text: string)=>{
+//     console.log("Event: ", text);
+//     return goHome();
+// })
+// .catch((err:any)=>{   //Catch for Reject value
+//     console.log("Error", err);
+// })
+// //console.log("Mechanics Response: ")
--- callback_promise/index.ts
+++ callback_promise/index.ts
@@ -1,39 +1,90 @@
-// Function to add two numbers and call a callback
-function addAsyncCallback(a: number, b: number, callback: (result: number) => void) {
-    function afterAddition(sum: number) {
-      callback(sum);
-    }
-  
+function carMaintenance1(cb:(text:string)=> void){
+    console.log("I am leaving a car to you")
+    setTimeout(()=>{
+        console.log( "car is ready")
+        cb("Your car is ready")
+    }, 2000);
+}
+function carMaintCallBack(text: string){
+    console.log("car maintenance callback: ", text);
+    pickDress1(dressCallBack);
+}
+
+function pickDress1(callback:(text:string)=> void){
     setTimeout(() => {
-      const sum = a + b;
-      afterAddition(sum);
+        console.log("your dress is ready")
+        callback("pick your dress")
     }, 1000);
-  }
-    function callbackHandler(result: number) {
-    console.log("Callback Example: The sum is:", result);
-  }
-  addAsyncCallback(2, 3, callbackHandler);
-  
-  // Function to add two numbers and return a Promise
-  function addAsyncPromise(a: number, b: number): Promise<number> {
-    return new Promise((resolve) => {
-      function afterAddition(sum: number) {
-        resolve(sum);
-      }
-  
-      setTimeout(() => {
-        const sum = a + b;
-        afterAddition(sum);
-      }, 1000); 
-    });
-  }
-  
-  function promiseHandler(result: number) {
-    console.log("Promise Example: The sum is:", result);
-  }
-  addAsyncPromise(2, 3)
-    .then(promiseHandler)
-    .catch((error) => {
-      console.error("Promise Example: Error:", error);
-    });
-  
+}
+function dressCallBack(text: string){
+    console.log("This is Dress Callback   ", text);
+    attendEvent1()
+}
+
+function attendEvent1(){
+    console.log("Now you can attend the event")
+}
+carMaintenance1(carMaintCallBack);
+
+
+// console.log("\n\n\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n")
+
+//=======================================================================================
+
+
+// console.log("Promise Example")
+// //Promises
+// //Inversion of Control
+// function carMaintenance(){
+//     console.log("I am leaving a car to you")
+//     let result: Promise<string> = new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             // console.log( "car is ready")
+//             resolve("your car is ready")
+//             //reject("Your car is not ready")
+//         }, 2000);
+//     })
+
+//     return result;
+    
+// }
+// function pickDress(){
+//     let promise: Promise<string> = new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             console.log("your dress is ready")
+//             resolve("pick your dress")
+//         }, 100);
+//     })
+//     return promise;
+// }
+// function attendEvent(){
+//     let promise: Promise<string> = new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             console.log("Attending Event");
+//             resolve("Attending Event")
+//         },2000);
+//     })
+//     return promise;
+// }
+// function goHome(){
+//     setTimeout(()=>{
+//         console.log("Going Home");
+//     },2000);
+// }
+// let mechanicResponse = carMaintenance();
+// mechanicResponse.then((text: string)=>{  //Then for Resolve value
+//     console.log("Mechanics Response: ", text)
+//     return pickDress();
+// })
+// .then((text: string)=>{
+//     console.log("Laundary Response: ",text);
+//     return attendEvent()
+// })
+// .then((text: string)=>{
+//     console.log("Event: ", text);
+//     return goHome();
+// })
+// .catch((err:any)=>{   //Catch for Reject value
+//     console.log("Error", err);
+// })
+// //console.log("Mechanics Response: ")
